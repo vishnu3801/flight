ca98ff60e2b3f69b2fe72754c0811cd7117a20b7
vishnu3801 <69002566+vishnu3801@users.noreply.github.com>
2021-01-11 16:50:46 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,213 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import sklearn
+import pickle
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor , ExtraTreesRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.model_selection import RandomizedSearchCV , train_test_split
+df = pd.read_excel(r"air_train.xlsx")
+
+
+df1 = pd.read_excel(r"air_test.xlsx")
+
+df.dropna(inplace = True)
+df1.dropna(inplace = True)
+
+df["Journey_month"] = pd.to_datetime(df["Date_of_Journey"]).dt.month
+df["Journey_day"] = pd.to_datetime(df["Date_of_Journey"]).dt.day
+
+df["Dep_Time_hr"] = pd.to_datetime(df["Dep_Time"]).dt.hour
+df["Dep_Time_min"] = pd.to_datetime(df["Dep_Time"]).dt.minute
+
+df["Arrival_Time_hr"] = pd.to_datetime(df["Arrival_Time"]).dt.hour
+df["Arrival_Time_min"] = pd.to_datetime(df["Arrival_Time"]).dt.minute
+
+#test
+
+df1["Journey_month"] = pd.to_datetime(df1["Date_of_Journey"]).dt.month
+df1["Journey_day"] = pd.to_datetime(df1["Date_of_Journey"]).dt.day
+
+df1["Dep_Time_hr"] = pd.to_datetime(df1["Dep_Time"]).dt.hour
+df1["Dep_Time_min"] = pd.to_datetime(df1["Dep_Time"]).dt.minute
+
+df1["Arrival_Time_hr"] = pd.to_datetime(df1["Arrival_Time"]).dt.hour
+df1["Arrival_Time_min"] = pd.to_datetime(df1["Arrival_Time"]).dt.minute
+
+
+df.drop(["Date_of_Journey" , "Dep_Time" , "Arrival_Time","Additional_Info","Route"] , axis =1 , inplace = True)
+df1.drop(["Date_of_Journey" , "Dep_Time" , "Arrival_Time","Additional_Info","Route"] , axis =1 , inplace = True)
+
+
+le = LabelEncoder()
+df["Source"] = le.fit_transform(df["Source"])
+df["Destination"] = le.fit_transform(df["Destination"])
+
+df1["Source"] = le.fit_transform(df1["Source"])
+df1["Destination"] = le.fit_transform(df1["Destination"])
+
+
+#mapping
+
+stop = {
+    "non-stop":0,
+    "1 stop":1,
+    "2 stops":2,
+    "3 stops":3,
+    "4 stops":4
+}
+
+df.loc[: , "Total_Stops"] = df["Total_Stops"].map(stop)
+
+df1.loc[: , "Total_Stops"] = df1["Total_Stops"].map(stop)
+
+
+#change of duration into hr and min
+#train
+
+duration = list(df["Duration"])
+
+for i in range(len(duration)):
+    if len(duration[i].split()) != 2:    # Check if duration contains only hour or mins
+        if "h" in duration[i]:
+            duration[i] = duration[i].strip() + " 0m"   # Adds 0 minute
+        else:
+            duration[i] = "0h " + duration[i]           # Adds 0 hour
+
+duration_hours = []
+duration_mins = []
+for i in range(len(duration)):
+    duration_hours.append(int(duration[i].split(sep = "h")[0]))    # Extract hours from duration
+    duration_mins.append(int(duration[i].split(sep = "m")[0].split()[-1]))
+
+    #test
+duration = list(df1["Duration"])
+
+for i in range(len(duration)):
+    if len(duration[i].split()) != 2:    # Check if duration contains only hour or mins
+        if "h" in duration[i]:
+            duration[i] = duration[i].strip() + " 0m"   # Adds 0 minute
+        else:
+            duration[i] = "0h " + duration[i]           # Adds 0 hour
+
+duration_hours_t = []
+duration_mins_t = []
+for i in range(len(duration)):
+    duration_hours_t.append(int(duration[i].split(sep = "h")[0]))    # Extract hours from duration
+    duration_mins_t.append(int(duration[i].split(sep = "m")[0].split()[-1]))
+
+df["Duration_hours"] = duration_hours
+df["Duration_mins"] = duration_mins
+
+df1["Duration_hours"] = duration_hours_t
+df1["Duration_mins"] = duration_mins_t
+
+df.drop(["Duration"] , axis =1 , inplace =True)
+df1.drop(["Duration"] , axis =1 , inplace =True)
+
+
+df = df[df.Airline != 'Trujet']
+
+df = df[df.Airline != 'Multiple carriers Premium economy']
+df = df[df.Airline != 'Jet Airways Business']
+df = df[df.Airline != 'Vistara Premium economy']
+
+
+
+df1 = df1[df1.Airline != 'Multiple carriers Premium economy']
+df1 = df1[df1.Airline != 'Jet Airways Business']
+df1 = df1[df1.Airline != 'Vistara Premium economy']
+
+
+#mapping
+
+stop = {
+    "Jet Airways":0,
+    "IndiGo":1,
+    "Air India":2,
+    "Multiple carriers":3,
+    "SpiceJet":4 , "Vistara":5 ,"Air Asia":6 , "GoAir":7, 
+}
+
+df.loc[: , "Airline"] = df["Airline"].map(stop)
+df1.loc[: , "Airline"] = df1["Airline"].map(stop)
+
+x = df.drop(["Price"] , axis =1)
+y = df.Price
+x_train , x_test , y_train , y_test = train_test_split(x,y,random_state = 100 , test_size = 0.3)
+
+df.dropna(inplace = True)
+df.isna().sum()
+
+feat = ExtraTreesRegressor()
+feat.fit(x_train , y_train)
+
+
+##create model
+lr = LinearRegression()
+xgb = XGBRegressor()
+rfr = RandomForestRegressor()
+dt = DecisionTreeRegressor()
+
+##hyperparameter tuning
+
+
+##hyper params tuning
+
+
+rf_p = {
+    
+    "min_samples_split": list(range(2,11)),
+    "min_samples_leaf" : list(range(1,10)),
+    "max_depth":list(range(1,200)),
+    "n_estimators": list(range(1,500))
+}
+
+dt_p = {
+    "criterion":["mse"],
+    "min_samples_split": list(range(2,11)),
+    "min_samples_leaf" : list(range(1,10)),
+    "max_depth":list(range(1,200))
+}
+
+xgb_p = {
+    "learning_rate" : [0.1,0.2,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.5],
+    "max_depth" : list(range(1,200)),
+    "booster" : ['gbtree', 'gblinear' ,'dart'],
+    "min_child_weight" : list(range(1,20)),
+    "n_estimators" : list(range(1,200))
+}
+
+rscv = RandomizedSearchCV(rfr , param_distributions=rf_p , cv =10 , n_iter=10  ,n_jobs = -1 , verbose = 10)
+
+rscv.fit(x,y)
+
+rscv.best_estimator_
+
+
+rfr =RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=35,
+                      max_features='auto', max_leaf_nodes=None,
+                      min_impurity_decrease=0.0, min_impurity_split=None,
+                      min_samples_leaf=3, min_samples_split=9,
+                      min_weight_fraction_leaf=0.0, n_estimators=69,
+                      n_jobs=None, oob_score=False, random_state=None,
+                      verbose=0, warm_start=False)
+
+rfr.fit(x_train , y_train)
+xgb.fit(x_train , y_train)
+dt.fit(x_train , y_train)
+
+#save the model
+
+
+pickle.dump(rfr,open('model.pkl','wb'))
